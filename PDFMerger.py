560bd225a41874305270c0ad885d0825f748b2c2
Electronica-dev <srsamartha@gmail.com>
2020-07-12 15:08:21 +0530

    Add .pdf check

--- PDFMerger.py
+++ PDFMerger.py
@@ -1,18 +1,21 @@
+
 # Merge pdf files in a folder (which contains ONLY pdf files).
 
-import PyPDF2
-import os
+from PyPDF2 import PdfFileMerger
+from os import listdir
+from os import path
 from datetime import date
 from natsort import natsorted
 
 # Get date in a recognizable format.
 dateToday = date.today().strftime("%d-%m-%Y")
 
-merger = PyPDF2.PdfFileMerger()
+merger = PdfFileMerger()
 
 
 def merge_pdf_in_folder(folder_path, output_path):
-    pdf_list = os.listdir(folder_path)
+    print('Creating combined pdf...')
+    pdf_list = listdir(folder_path)
 
     file = []  # Creating empty list to store the file paths.
 
@@ -26,9 +29,10 @@ def merge_pdf_in_folder(folder_path, output_path):
         merger.append(PDF)
 
     # Opening the file with the name containing today's date.
-    file_path = open(os.path.join(output_path, 'Prajavani ' + dateToday + '.pdf'), 'wb')
+    file_path = open(path.join(output_path, 'Prajavani ' + dateToday + '.pdf'), 'wb')
 
     merger.write(file_path)  # Writing to the file.
 
     file_path.close()
     merger.close()
+    print('PDF created at: ' + output_path + '/Prajavani ' + str(dateToday) + '.pdf')
