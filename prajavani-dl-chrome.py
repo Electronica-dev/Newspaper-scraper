23363009245aa71a3b601d2cd9dd90487d55d5e7
Sammy <45600001+Electronica-dev@users.noreply.github.com>
2020-07-17 18:20:42 +0530

    Update prajavani-dl-chrome.py

--- prajavani-dl-chrome.py
+++ prajavani-dl-chrome.py
@@ -32,9 +32,9 @@ desired_cap = {
     'name': "1-first-test-Windows-Chrome",
 }
 
-username = os.environ["SAUCE_USERNAME"]
-access_key = os.environ["SAUCE_ACCESS_KEY"]
-capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+username = environ["SAUCE_USERNAME"]
+access_key = environ["SAUCE_ACCESS_KEY"]
+capabilities["tunnel-identifier"] = environ["TRAVIS_JOB_NUMBER"]
 hub_url = "%s:%s@localhost:4445" % (username, access_key)
 driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
 
